5e7b2e52eaefc3966b7b70b165aaf7a6c854b37b
github <144250585+githubbs7@users.noreply.github.com>
2024-12-08 12:47:21 +0500

    Update app.js
    Update api address

--- public/app.js
+++ public/app.js
@@ -4,7 +4,7 @@ let video = document.querySelector('video')
 video.style.display = 'none'
 
 btn.addEventListener('click', async function da() {
-    let api = await fetch(`http://localhost:3000/download-tiktok?url=${user.value}`)
+    let api = await fetch(`https://no-watermark.vercel.app/download-tiktok?url=${user.value}`);
     let data = await api.json()
     video.style.display = 'block'
     video.src = data.videoUrl
